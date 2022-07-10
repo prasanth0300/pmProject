1c1adda67787cab2ba47161b7d2f1f77801a0a9e
prasanth0300 <prasanthcena3@gmail.com>
2022-07-10 19:22:55 +0530

    prasanth

---
+++ src/org/sample/git_project_class.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class git_project_class {
+
+	private void test1() {
+		System.out.println("test1");
+	}
+
+	private void test2() {
+		System.out.println("test2");
+	}
+}
